4c3626857e84d76b7082553685a58c9e4a759369
reddy <reddy@bunny>
2017-02-06 14:05:29 +0530

    added a new string

---
+++ src/GoodMorning.java
@@ -0,0 +1,10 @@
+
+public class GoodMorning {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("good morning");
+System.out.println("have a nice day");
+	}
+
+}
